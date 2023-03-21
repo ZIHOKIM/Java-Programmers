5605a55195ca911e6a1b1d87b3b846cefcdf8c95
jiho <wlgj0802@naver.com>
2023-03-21 18:42:27 +0900

    babbling

---
+++ Babbling.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] babbling) {
+        
+        int result = 0;
+        String[] words = {"aya", "ye", "woo", "ma"};
+        
+        for(int i=0; i<babbling.length; i++){
+            int wordCount = 0;
+            String babblingWord = babbling[i]; 
+            
+            for(int j=0; j<words.length; j++){
+                if(babblingWord.contains(words[j])){
+                    wordCount += words[j].length();
+                }
+            }
+            if(babblingWord.length() == wordCount){
+                result++;
+            }
+        }
+        return result;
+    }
+        
+}

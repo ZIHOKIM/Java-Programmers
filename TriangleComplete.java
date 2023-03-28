2f957dc0862dfd8db0b2eff07f8f5babe3fe7ed4
jiho <wlgj0802@naver.com>
2023-03-28 17:58:54 +0900

    .

---
+++ TriangleComplete.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] sides) {
+        int answer = 1;
+        Arrays.sort(sides);
+        int sum = sides[0]+sides[1];
+        if(sides[2]>=sum){
+            answer = 2;
+        }
+        return answer;
+    }
+}
--- zz.java
+++ zz.java
@@ -1,5 +1,6 @@
-public class New{
+public class zz{
     public static void main(String[] args) {
-        System.out.println("hello World");
+        System.out.println("hello World~~~~~~~~~");
+        System.out.println("오랜만에 코딩ㅋ 정신차려 지오!");
     }
 }

1b787eb7668dc5c4cdb133f44e561ee240ef2bd1
ZIHOKIM <wlgj0802@naver.com>
2023-05-31 13:19:33 +0900

    .

--- OddEvenCount.java
+++ OddEvenCount.java
@@ -16,5 +16,13 @@ public class OddEvenCount {
             answer[1] = odd;
             return answer;
         }
+
+        public int[] solution2(int[] num_list) {
+            int[] answer = {};
+            for(int i = 0; i < num_list.length; i++){
+                answer[num_list[i]%2]++;
+            }
+            return answer;
+        }
     }
 }
